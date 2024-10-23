42a72c096e959c33163085154b3c994974ecc9cc
inan <inanygt@hotmail.com>
2024-10-23 16:24:52 +0200

    add first gsap animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'optionFive';
+
+  animateSubheading() {
+    gsap.from('.subheading', {
+      x: -300,
+      duration: 1,
+      ease: 'power2.inOut',
+    });
+  }
+
+  ngOnInit(): void {
+    this.animateSubheading();
+  }
 }
