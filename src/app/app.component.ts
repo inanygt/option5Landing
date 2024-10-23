3d2c78bf845f25b7fec267b7e2a09d441bcd0fc1
inan <inanygt@hotmail.com>
2024-10-23 17:03:41 +0200

    add gsap animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,42 @@ export class AppComponent implements OnInit {
     gsap.from('.subheading', {
       x: -300,
       duration: 1,
+      opacity: 0,
       ease: 'power2.inOut',
+    });
+  }
+
+  animateSubscribeButton() {
+    gsap.from('input', {
+      y: 300,
+      duration: 1,
+      delay: 0.5,
+      ease: 'power2.inOut',
+      opacity: 0,
+      stagger: 0.5,
+    });
+  }
+
+  animateLandingImage() {
+    gsap.from('.img_landing', {
+      x: 200,
+      ease: 'elastic.out',
+      duration: 3,
+    });
+  }
+
+  animateSocialMediaIcons() {
+    gsap.from('.socials-container img', {
+      x: 300,
+      duration: 1,
+      stagger: 0.3,
     });
   }
 
   ngOnInit(): void {
     this.animateSubheading();
+    this.animateSubscribeButton();
+    this.animateLandingImage();
+    this.animateSocialMediaIcons();
   }
 }
