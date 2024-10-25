9b06deadadd041c21b07d1f3d6efdd6a16443543
inan <inanygt@hotmail.com>
2024-10-25 16:24:13 +0200

    edit readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import gsap from 'gsap';
+import { TextPlugin } from 'gsap/all';
+
+gsap.registerPlugin(TextPlugin);
 
 @Component({
   selector: 'app-root',
@@ -29,25 +32,100 @@ export class AppComponent implements OnInit {
 
   animateSubheading() {
     gsap.from('.subheading', {
+      delay: 2,
       x: -300,
-      duration: 4,
+      duration: 2,
       opacity: 0,
-      ease: 'elastic.out',
     });
   }
 
   animateSocialMediaIcons() {
     gsap.from('.logo', {
-      delay: 1,
+      delay: 2,
       y: 100,
-      duration: 1,
+      duration: 2,
       stagger: 0.3,
       opacity: 0,
     });
   }
 
+  startCounterAnimation() {
+    const tl = gsap.timeline();
+
+    tl.set('.logo_option5', {
+      display: 'none',
+    });
+    tl.to('#counter', {
+      text: '000',
+      duration: 0.6,
+      ease: 'none',
+    })
+      .to('#counter', {
+        text: '010',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '020',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '030',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '040',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '050',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '060',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '070',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '080',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '090',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        text: '100',
+        duration: 0.1,
+        ease: 'none',
+      })
+      .to('#counter', {
+        y: -300,
+        ease: 'power1.inOut',
+        opacity: 0,
+      })
+      .from('.logo_option5', {
+        display: 'block',
+        x: -200,
+        duration: 2,
+        opacity: 0,
+      });
+  }
+
   ngOnInit(): void {
     this.animateSubheading();
     this.animateSocialMediaIcons();
+    this.startCounterAnimation();
   }
 }
